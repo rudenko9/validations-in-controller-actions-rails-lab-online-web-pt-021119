10ec7ad19766e7d571f97930593e08b1854b8fbf
Nataliya R <n.rudenko@gmail.com>
2019-07-22 19:14:04 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true 
-  validates :content , length: {is: }
+  #validates :content , length: {is: }
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
