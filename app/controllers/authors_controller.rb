09c8486d11fc91271f97a32033947fa870026c31
Nataliya R <n.rudenko@gmail.com>
2019-07-22 19:24:35 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,12 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
+    @author = Author.new(author_params)
     if @author.valid?
-      @auth
+      @author.save
+    else 
+      render :new 
+    end 
 
    # redirect_to author_path(@author)
   end
