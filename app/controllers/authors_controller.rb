8139487ed82650d6b62d2ba1e6ebcf13345d348c
Nataliya R <n.rudenko@gmail.com>
2019-07-22 19:25:23 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,7 +11,7 @@ class AuthorsController < ApplicationController
     if @author.valid?
       @author.save
     else 
-      render :new 
+      render :edit 
     end 
 
    # redirect_to author_path(@author)
